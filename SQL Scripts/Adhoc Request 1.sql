35155c8d6814b06bd9156d3c57e7a9d03b2b0798
Pradnya Taras <161185363+pradnya0797@users.noreply.github.com>
2024-09-19 21:54:50 +0530

    Update Adhoc Request 1.sql

--- SQL Scripts/Adhoc Request 1.sql
+++ SQL Scripts/Adhoc Request 1.sql
@@ -1,12 +1,11 @@
 /*  1. Provide a list of products with a base price greater than 500 and 
-that are featured in promo type of 'BOGOF' (Buy One Get One Free). 
-This information will help us identify high-value products that are currently being heavily discounted, 
-which can be useful for evaluating our pricing and promotion strategies. */
+that are featured in promo type of 'BOGOF' (Buy One Get One Free).  */
 
 
-select distinct f.product_code, p.product_name, base_price, f.promo_type 
-from fact_events f 
-join dim_products as p 
-on f.product_code = p.product_code 
-where base_price > 500 and promo_type = "BOGOF" 
-
+SELECT DISTINCT p.product_name,
+                f.base_price
+FROM   fact_events f
+       JOIN dim_products p
+         ON f.product_code = p.product_code
+WHERE  f.promo_type = 'BOGOF'
+       AND f.base_price > 500;
